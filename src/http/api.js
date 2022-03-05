d58544ceaef6007b939df48dcb386f55e837ea85
LouKai <loukai0099@gmail.com>
2022-03-06 03:38:27 +0800

    Ver.0.1

---
+++ src/http/api.js
@@ -0,0 +1,5 @@
+import * as kgManager from './modules/kgmanager'
+
+export default {
+  kgManager
+}
---
+++ src/utils/global.js
@@ -0,0 +1,7 @@
+// 后台服务器地址
+export const baseUrl = 'http://localhost:6080'
+// export const baseUrl = 'http://localhost:8001'
+
+export default {
+  baseUrl,
+}
